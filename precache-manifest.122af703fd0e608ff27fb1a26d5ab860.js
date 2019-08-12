80db5de3e886265dcf53ae948c4565538267978d
Mathew, Anish K <anish.k.mathew@accenture.com>
2019-08-11 19:12:55 -0500

    Updates

---
+++ precache-manifest.122af703fd0e608ff27fb1a26d5ab860.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955853e961be8bf8b4a93dda8ee5469e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cbb919c3f00b48e6be05",
+    "url": "/monsters-rolodex/static/css/main.83283d4e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cbb919c3f00b48e6be05",
+    "url": "/monsters-rolodex/static/js/main.19f04d78.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
